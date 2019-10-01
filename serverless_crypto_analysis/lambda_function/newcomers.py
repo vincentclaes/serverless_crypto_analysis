482b2f784f1ce6260130203d6ec47e6d87efc082
vincent <vclaes1986@gmail.com>
2019-10-01 21:00:41 +0200

    add newcomers code and tests

--- serverless_crypto_analysis/lambda_function/newcomers.py
+++ serverless_crypto_analysis/lambda_function/newcomers.py
@@ -1,34 +1,23 @@
-import boto3
+# import boto3
 import os
 import pyathena
 import pandas as pd
+
 def handler(event, context):
     os.environ['AWS_PROFILE']='serverless'
-    # client = boto3.client('athena')
-
-    # queryStart = client.start_query_execution(
-    #     QueryString = 'SELECT * FROM coinmarketcap_data',
-    #     QueryExecutionContext = {
-    #         'Database': 'coinmarketcap'
-    #     },
-    #     ResultConfiguration={
-    #         'OutputLocation': 's3://aws-athena-query-results-077590795309-eu-central-1'
-    #     }   
-    # )
-
-    # result = client.get_query_results(
-    # QueryExecutionId=queryStart['QueryExecutionId']
-    #  )
-
+    rank = 100
     from pyathena import connect
 
-    cursor = connect(profile_name='serverless', schema_name='coinmarketcap', s3_staging_dir='s3://aws-athena-query-results-077590795309-eu-central-1').cursor()
-    conn = connect(profile_name='serverless', schema_name='coinmarketcap', s3_staging_dir='s3://aws-athena-query-results-077590795309-eu-central-1')
-    # cursor.execute("SELECT * FROM coinmarketcap_data")
+    conn = get_connection()
+    max_uuid = get_max_uuid(conn)
+    df_last = get_last_df(conn, uuid)
+    df_tail = get_tail_df(conn, uuid, rank)
+    newcomers = get_newcomers(df_last, df_tail)
+    return newcomers
+    # cursor.fetchall()
     # print(cursor.description)
     # print(cursor.fetchall())
-    df = pd.read_sql("SELECT * FROM coinmarketcap_data", conn)
-    df
+    # df = pd.read_sql("SELECT * FROM coinmarketcap_data", conn)
     # get latest uuid
     # SELECT max(uuid) FROM "coinmarketcap"."coinmarketcap_data";
     
@@ -40,6 +29,19 @@ def handler(event, context):
 
     # write a key to s3?
     # if there is at least one you can trigger another lambda
-    pass
 
-handler(None, None)
+def get_connection():
+    return connect(profile_name='serverless', schema_name='coinmarketcap', s3_staging_dir='s3://aws-athena-query-results-077590795309-eu-central-1')
+
+def get_max_uuid(conn):
+    return conn.cursor().execute("SELECT max(uuid) FROM coinmarketcap_data limit 1").fetchone()[0]
+
+def get_last_df(conn, uuid):
+    return pd.read_sql("SELECT * FROM crypto_data where uuid=={}".format(uuid))
+
+def get_tail_df(conn, uuid, rank):
+    return pd.read_sql("SELECT DISTINCT id FROM crypto_data where uuid<{} and rank <= {}".format(uuid, rank))
+
+def get_newcomers(df_last, df_tail):
+    newcomers = df_last[~df_last["id"].isin(df_tail["id"])]
+    return newcomers
---
+++ serverless_crypto_analysis_tests/__init__.py
@@ -0,0 +1,3 @@
+import os
+
+TEST_ROOT = os.path.dirname(os.path.realpath(__file__))
